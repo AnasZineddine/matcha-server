5e64ed961c4094e50c02a65a7f74811c7f3861ba
Anas Zineddine <azineddi@e1r6p10.1337.ma>
2021-05-07 15:18:58 +0000

    fix port

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const resolvers = require("./graphql/resolvers");
 
 const pubsub = new PubSub();
 
+const port = process.env.PORT || 5000;
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: ({ req }) => ({ req, pubsub }),
 });
 
-server.listen({ port: 5000 }).then((res) => {
+server.listen({ port: port }).then((res) => {
   console.log(`Server running at ${res.url}`);
 });
