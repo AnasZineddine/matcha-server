ab414b9848378ec88b584c942dc9e22e7786b377
AnasZineddine <a.zineddine2011@gmail.com>
2021-05-07 21:42:25 +0000

    fix database url

--- db.js
+++ db.js
@@ -2,7 +2,6 @@ const Pool = require("pg").Pool;
 require("dotenv").config();
 
 const pool = new Pool({
-  DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
 });
-
 module.exports = pool;
