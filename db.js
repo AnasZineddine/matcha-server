44a9192b70146bce2c5142c9d553e6baa886d1e0
AnasZineddine <a.zineddine2011@gmail.com>
2021-05-07 22:14:37 +0000

    fix ssl postgres

--- db.js
+++ db.js
@@ -3,6 +3,6 @@ require("dotenv").config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: true,
+  ssl: { rejectUnauthorized: false },
 });
 module.exports = pool;
