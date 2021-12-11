c5a8209f2de5d41327661689a197f06b228edc61
Anas Zineddine <azineddi@e1r6p10.1337.ma>
2021-12-11 16:00:56 +0100

    log user

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -914,10 +914,10 @@ module.exports = {
       const user = await checkAuth(context);
       try {
         const lastSeen = await pool.query(
-          "UPDATE users SET user_last_connected = current_timestamp + (60 * interval '1 minute') WHERE user_id = $1",
+          "UPDATE users SET user_last_connected = current_timestamp + (60 * interval '1 minute') WHERE user_id = $1 RETURNING user",
           [user.id]
         );
-        console.log({ lastSeen });
+        console.log(lastSeen);
         return true;
       } catch (error) {
         console.log(error);
